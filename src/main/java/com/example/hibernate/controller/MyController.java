b2a375d027ba86d72acb4ff1e29c04a4b6241a62
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2022-04-30 15:00:58 +0300

    method-security

--- src/main/java/com/example/hibernate/controller/MyController.java
+++ src/main/java/com/example/hibernate/controller/MyController.java
@@ -1,16 +1,17 @@
 package com.example.hibernate.controller;
 
-import com.example.hibernate.entity.Persons;
 import com.example.hibernate.service.MyService;
 import com.google.gson.Gson;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -22,7 +23,14 @@ public class MyController {
         this.service = service;
     }
 
+    @GetMapping("/hello")
+    @PreAuthorize("authentication.principal.username == #name")
+    public String hello(@RequestParam(required = false, defaultValue = "guest") String name){
+        return "Hello ";
+    }
+
     @GetMapping("/by-city")
+    @Secured("ROLE_READ")
     public void city(@RequestParam(required = false) String city, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         final var data = service.getPersonsByCity(city);
@@ -31,6 +39,7 @@ public class MyController {
     }
 
     @GetMapping("/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public void age(@RequestParam(required = false) int age, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         final var data = service.getPersonsByAge(age);
@@ -39,6 +48,7 @@ public class MyController {
     }
 
     @GetMapping("/by-name-surname")
+    @PreAuthorize("hasRole('WRITE') or hasRole('DELETE')")
     public void nameSurname(@RequestParam(required = false) String name, String surname, HttpServletResponse response) throws Exception {
         response.setContentType("application/json");
         final var data = service.getPersonsByNameSurname(name, Optional.ofNullable(surname).orElse("Sidorov"));
--- src/main/java/com/example/hibernate/security/SecurityConfig.java
+++ src/main/java/com/example/hibernate/security/SecurityConfig.java
@@ -2,27 +2,26 @@ package com.example.hibernate.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth.inMemoryAuthentication().withUser("lida").password(encoder.encode("1234")).roles("ADMIN")
-                .and().withUser("vlad").password(encoder.encode("4321")).roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("lida").password(encoder.encode("1234")).roles("READ")
+                .and().withUser("vlad").password(encoder.encode("1234")).roles("WRITE")
+                .and().withUser("petya").password(encoder.encode("1234")).roles("DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().mvcMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+        http.formLogin().and().authorizeRequests().anyRequest().authenticated();
     }
 }
